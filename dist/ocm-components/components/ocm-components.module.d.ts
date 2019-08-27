6ab0fdaa275523ff2e5aad010eaccc562b35b54f
Oliver Heywood <oheywood@four51.com>
2019-08-27 09:26:46 -0500

    trying to get library to work without /dist again

--- dist/ocm-components/components/ocm-components.module.d.ts
+++
@@ -1,6 +0,0 @@
-import { Injector } from '@angular/core';
-export declare class OcmComponentsModule {
-    private injector;
-    constructor(injector: Injector);
-    buildWebComponent(angularComponent: any, htmlTagName: string): void;
-}
--- dist/ocm-components/esm2015/public-api.js
+++
@@ -1,9 +0,0 @@
-/**
- * @fileoverview added by tsickle
- * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
- */
-/*
- * Public API Surface of ocm-components
- */
-export { OcmComponentsModule } from './components/ocm-components.module';
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoicHVibGljLWFwaS5qcyIsInNvdXJjZVJvb3QiOiJuZzovL29jbS1jb21wb25lbnRzLyIsInNvdXJjZXMiOlsicHVibGljLWFwaS50cyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiOzs7Ozs7O0FBSUEsb0NBQWMsb0NBQW9DLENBQUMiLCJzb3VyY2VzQ29udGVudCI6WyIvKlxuICogUHVibGljIEFQSSBTdXJmYWNlIG9mIG9jbS1jb21wb25lbnRzXG4gKi9cblxuZXhwb3J0ICogZnJvbSAnLi9jb21wb25lbnRzL29jbS1jb21wb25lbnRzLm1vZHVsZSc7XG4iXX0=
